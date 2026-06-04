4c0bea16c91ab7a920d11e73f5874ec802c3a2c0
CrabbyAlex63 <alexander.cottier@education.nsw.gov.au>
2026-06-04 15:02:56 +1000

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -29,28 +29,28 @@ while True:
     left = robot.line_left()     # 0 = black, 1 = white
     right = robot.line_right()   # 0 = black, 1 = white
 
-    # --- BOTH BLACK → GO STRAIGHT ---
+    # BOTH BLACK = GO STRAIGHT
     if left == 0 and right == 0:
         L = FORWARD #- BIAS
         R = FORWARD
         LD = 1
         RD = 0
 
-    # --- LEFT WHITE → TURN RIGHT (correct direction) ---
+    # LEFT WHITE = TURN RIGHT (correct direction) 
     elif left == 0 and right == 1:
         L = TURN
         R = TURN
         LD = 1      # left wheel backward
         RD = 0      # right wheel forward
 
-    # --- RIGHT WHITE → TURN LEFT (correct direction) ---
+    # RIGHT WHITE = TURN LEFT (correct direction)
     elif left == 1 and right == 0:
         L = TURN
         R = TURN
         LD = 0      # left wheel forward
         RD = 1      # right wheel backward
 
-    # --- BOTH WHITE → KEEP LAST ACTION ---
+    # BOTH WHITE = KEEP LAST ACTION 
     else:
         L = last_left_speed
         R = last_right_speed
