dd69ff75c92b9595aaa32a591bb1f474d2416c28
Jack <jack.clayton2009@icloud.com>
2026-07-23 10:01:58 +1000

    Fixed the non reversing issue (probably)

--- Code/main.py
+++ Code/main.py
@@ -8,12 +8,14 @@ import utime
 
 robot = Maqueen()
 
-FORWARD = 65
-TURN_FAST =20   # boost outer wheel for sharper correction
-TURN_SLOW = 0     # stop inner wheel completely for aggressive turn
+FORWARD = 90
+TURN_FAST = 60    # outer wheel speed during turn
+TURN_SLOW = 40    # inner wheel REVERSED for sharper pivot
 
 last_left_speed = FORWARD
 last_right_speed = FORWARD
+last_left_dir = 0
+last_right_dir = 0
 
 smirk = Image(
     "00000:"
@@ -26,37 +28,39 @@ smirk = Image(
 display.show(smirk)
 
 while True:
-    left = robot.line_left()     # 0 = black, 1 = white
-    right = robot.line_right()   # 0 = black, 1 = white
+    left = robot.line_left()     # Maybe: 1 = black, 0 = white?
+    right = robot.line_right()
 
     # --- BOTH BLACK → GO STRAIGHT ---
-    if left == 0 and right == 0:
-        L = FORWARD
-        R = FORWARD
+    if right == 1 and left == 1:
+        L_speed, L_dir = FORWARD, 0
+        R_speed, R_dir = FORWARD, 0
 
     # --- LEFT BLACK, RIGHT WHITE → VEER LEFT ---
-    elif left == 0 and right == 1:
-        L = TURN_SLOW
-        robot.motor_left(TURN_SLOW, 1)
-        R = TURN_FAST
+    elif right == 1 and left == 0:
+        L_speed, L_dir = TURN_SLOW, 1
+        R_speed, R_dir = TURN_FAST, 0
 
     # --- LEFT WHITE, RIGHT BLACK → VEER RIGHT ---
-    elif left == 1 and right == 0:
-        L = TURN_FAST
-        robot.motor_right(TURN_SLOW, 1)
-        R = TURN_SLOW
+    elif right == 0 and left == 1:
+        L_speed, L_dir = TURN_FAST, 0
+        R_speed, R_dir = TURN_SLOW, 1
 
-    # --- BOTH WHITE → KEEP LAST ACTION ---
+    # --- BOTH WHITE → REVERSE---
     else:
-        L = last_left_speed
-        R = last_right_speed
+        R_speed, R_dir = TURN_SLOW, 1
+        L_speed, L_dir = TURN_SLOW, 1
 
-    # Both wheels always go forward (direction = 0)
-    robot.motor_left(L, 0)
-    robot.motor_right(R, 0)
+    # Apply motor commands
+    robot.motor_left(L_speed, L_dir)
+    utime.sleep_ms(1)
+    robot.motor_right(R_speed, R_dir)
 
-    last_left_speed = L
-    last_right_speed = R
+    # Store current values (but NOT when both white)
+    if not (right == 0 and left == 0):
+        last_left_speed = L_speed
+        last_right_speed = R_speed
+        last_left_dir = L_dir
+        last_right_dir = R_dir
 
     utime.sleep_ms(10)
-
