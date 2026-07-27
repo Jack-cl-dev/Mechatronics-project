d8a760c67de315b994d2e9499def2281822971d0
CrabbyAlex63 <alexander.cottier@education.nsw.gov.au>
2026-07-27 12:56:16 +1000

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,72 +1,62 @@
 from microbit import *
 from maqueen import Maqueen
 from sound_detect import SoundSwitch
+from obstacle_detect import ObstacleDetector
 import utime
 
 robot = Maqueen()
 sound_switch = SoundSwitch()
+detector = ObstacleDetector(robot, stop_distance=10)
 
 FORWARD = 90
 TURN_FAST = 60
 TURN_SLOW = 40
 
-last_left_speed = FORWARD
-last_right_speed = FORWARD
-last_left_dir = 0
-last_right_dir = 0
-
-display.show(Image.HAPPY)
-
 while True:
-    #  SOUND TOGGLE CHECK
-    wheels_on = sound_switch.update()
+    motors_running = sound_switch.state
+    horn_active = detector.horn_active
 
-    # MOTOR ON/OFF CONTROL 
+    wheels_on = sound_switch.update(motors_running, horn_active)
+
     if not wheels_on:
-        # Motors OFF
         robot.motor_left(0, 0)
         robot.motor_right(0, 0)
-        display.show("0")
         utime.sleep_ms(40)
         continue
-    else:
-        display.show("1")
 
-    # LINE SENSORS 
+    if detector.check():
+        detector.react()
+        continue
+
     left = robot.line_left()
     right = robot.line_right()
 
-    # BOTH BLACK → GO STRAIGHT
     if right == 1 and left == 1:
-        L_speed, L_dir = FORWARD, 0
-        R_speed, R_dir = FORWARD, 0
+        L_speed = FORWARD
+        L_dir = 0
+        R_speed = FORWARD
+        R_dir = 0
 
-    #  LEFT BLACK, RIGHT WHITE → VEER LEFT 
     elif right == 1 and left == 0:
-        L_speed, L_dir = TURN_SLOW, 1
-        R_speed, R_dir = TURN_FAST, 0
+        L_speed = TURN_SLOW
+        L_dir = 1
+        R_speed = TURN_FAST
+        R_dir = 0
 
-    # LEFT WHITE, RIGHT BLACK = VEER RIGHT 
     elif right == 0 and left == 1:
-        L_speed, L_dir = TURN_FAST, 0
-        R_speed, R_dir = TURN_SLOW, 1
+        L_speed = TURN_FAST
+        L_dir = 0
+        R_speed = TURN_SLOW
+        R_dir = 1
 
-    # BOTH WHITE = REVERSE 
     else:
-        R_speed, R_dir = TURN_SLOW, 1
-        L_speed, L_dir = TURN_SLOW, 1
+        R_speed = TURN_SLOW
+        R_dir = 1
+        L_speed = TURN_SLOW
+        L_dir = 1
 
-    # APPLY MOTOR COMMANDS 
     robot.motor_left(L_speed, L_dir)
     utime.sleep_ms(1)
     robot.motor_right(R_speed, R_dir)
 
-    # Store last values (except both white)
-    if not (right == 0 and left == 0):
-        last_left_speed = L_speed
-        last_right_speed = R_speed
-        last_left_dir = L_dir
-        last_right_dir = R_dir
-
     utime.sleep_ms(10)
-
