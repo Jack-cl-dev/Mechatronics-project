21717887beb54464bd0b3fd763c1cc38114a73ec
CrabbyAlex63 <alexander.cottier@education.nsw.gov.au>
2026-07-30 14:40:07 +1000

    Update sound_detect.py
    implementing sound into the working main

--- Code/sound_detect.py
+++ Code/sound_detect.py
@@ -9,15 +9,9 @@ class SoundSwitch:
         self.state = False
         self.last_trigger = 0
 
-    def update(self, motors_running, horn_active):
+    def update(self):
         level = microphone.sound_level()
         now = utime.ticks_ms()
-
-        if motors_running:
-            return self.state
-
-        if horn_active:
-            return self.state
 
         if level > CLAP_THRESHOLD and (now - self.last_trigger) > DEBOUNCE_TIME_MS:
             self.state = not self.state
