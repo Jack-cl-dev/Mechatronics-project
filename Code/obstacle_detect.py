148ac3139a88f3720505454a12e0acd971bbdc75
CrabbyAlex63 <alexander.cottier@education.nsw.gov.au>
2026-08-03 12:33:09 +1000

    Enhance obstacle detection
    Update obstacle detection logic to require multiple confirmations before acting.

--- Code/obstacle_detect.py
+++ Code/obstacle_detect.py
@@ -8,22 +8,24 @@ class ObstacleDetector:
         self.stop_distance = stop_distance
         self.horn_duration = 2000
         self.horn_active = False
+        self.close_streak = 0
+        self.confirm_needed = 3   # readings in a row before we trust it
 
     def check(self):
         distance = self.robot.ultrasound_measure()
 
-        if distance <= 0:
+        if distance <= 0 or distance > self.stop_distance:
+            self.close_streak = 0
             return False
 
-        return distance <= self.stop_distance
+        self.close_streak += 1
+        return self.close_streak >= self.confirm_needed
 
     def react(self):
         self.horn_active = True
-
         self.robot.motor_left(0, 0)
         self.robot.motor_right(0, 0)
-
-        audio.play(audio.Sound.SAD)
+        audio.play(Sound.SAD)
         utime.sleep_ms(self.horn_duration)
-
         self.horn_active = False
+        self.close_streak = 0
