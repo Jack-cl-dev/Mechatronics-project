d4cf94dc314f032136248b562d5037d7a05b61ab
CrabbyAlex63 <alexander.cottier@education.nsw.gov.au>
2026-08-06 10:08:28 +1000

    removal of broken code
    I just removed the broken obstacle avoidance because it ws messing with the headlights

--- Code/obstacle_detect.py
+++ Code/obstacle_detect.py
@@ -1,44 +1,31 @@
 from microbit import *
 import utime
+import audio
 
-CLAP_MARGIN = 70
-MIN_CLAP_LEVEL = 200
-MIN_STATE_TIME_MS = 1500     # hard lockout: can't re-toggle sooner than this
-QUIET_TIME_NEEDED_MS = 300   # must be quiet for this long (real time) to re-arm
+class ObstacleDetector:
+    def __init__(self, robot, stop_distance=10):
+        self.robot = robot
+        self.stop_distance = stop_distance
+        self.horn_duration = 2000
+        self.horn_active = False
+        self.close_streak = 0
+        self.confirm_needed = 3   # readings in a row before we trust it
 
-class SoundSwitch:
-    def __init__(self):
-        self.state = False
-        self.last_trigger = 0
-        self.armed = True
-        self.quiet_since = None
-        self.baseline = microphone.sound_level()
+    def check(self):
+        distance = self.robot.ultrasound_measure()
 
-    def update(self):
-        sound = microphone.sound_level()
-        now = utime.ticks_ms()
-        threshold = max(MIN_CLAP_LEVEL, self.baseline + CLAP_MARGIN)
+        if distance <= 0 or distance > self.stop_distance:
+            self.close_streak = 0
+            return False
 
-        if self.armed:
-            if sound > threshold and utime.ticks_diff(now, self.last_trigger) > MIN_STATE_TIME_MS:
-                self.state = not self.state
-                self.last_trigger = now
-                self.armed = False
-                self.quiet_since = None
-            else:
-                # track ambient baseline only while calm/armed
-                self.baseline += (sound - self.baseline) * 0.05
-        else:
-            # can't even consider re-arming until the lockout time has passed
-            if utime.ticks_diff(now, self.last_trigger) < MIN_STATE_TIME_MS:
-                self.quiet_since = None
-            elif sound < threshold:
-                if self.quiet_since is None:
-                    self.quiet_since = now
-                elif utime.ticks_diff(now, self.quiet_since) >= QUIET_TIME_NEEDED_MS:
-                    self.armed = True
-                    self.quiet_since = None
-            else:
-                self.quiet_since = None
+        self.close_streak += 1
+        return self.close_streak >= self.confirm_needed
 
-        return self.state
+    def react(self):
+        self.horn_active = True
+        self.robot.motor_left(0, 0)
+        self.robot.motor_right(0, 0)
+        audio.play(Sound.SAD)
+        utime.sleep_ms(self.horn_duration)
+        self.horn_active = False
+        self.close_streak = 0
